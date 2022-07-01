29d08c1aacb1bae117a6f4d8c0b4bd7910683bb7
willian portela <willianportela1@gmail.com>
2022-07-01 19:15:50 -0300

    req 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,73 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      albumName: '',
+      loading: false,
+      musicResults: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  fetchApi = () => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    this.setState({ loading: true }, async () => {
+      const results = await getMusics(id);
+      this.setState({
+        artistName: results[0].artistName,
+        albumName: results[0].collectionName,
+        musicResults: results,
+        loading: false,
+      });
+    });
+  };
+
   render() {
+    const { musicResults, artistName, albumName, loading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <p data-testid="artist-name">{artistName}</p>
+            <p data-testid="album-name">{albumName}</p>
+            {musicResults
+              .filter((music) => music.kind === 'song')
+              .map((music, index) => (
+                <MusicCard
+                  key={ index }
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                />
+              ))}
+          </div>
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
