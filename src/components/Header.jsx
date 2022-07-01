bf2d0768e85fb0b2328fc70804dc3225956187a4
willian portela <willianportela1@gmail.com>
2022-07-01 16:05:27 -0300

    req 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -21,7 +22,14 @@ export default class Header extends Component {
     const { user, loading } = this.state;
     return (
       <div data-testid="header-component">
-        {loading ? <Loading /> : <p data-testid="header-user-name">{user.name}</p>}
+        {loading ? (
+          <Loading />
+        ) : (
+          <p data-testid="header-user-name">{user.name}</p>
+        )}
+        <Link data-testid="link-to-search" to="/search">search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </div>
     );
   }
